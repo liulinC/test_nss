a909bb6647718aa34348965431ebe3e1a9aa682d
Lin Liu <lin.liu@citrix.com>
2021-03-11 09:55:03 +0000

    Remove unnecessary codes
    Signed-off-by: Lin Liu <lin.liu@citrix.com>

--- hcp_nss.c
+++ hcp_nss.c
@@ -1,39 +1,9 @@
 #include "hcp_nss.h"
-#include "winbind_nss_linux.h"
-#include <nss.h>
-#include <pwd.h>
 
-/*
-struct passwd rootpw_static = {
-	.pw_name = "root",
-	.pw_passwd = "x",
-	.pw_gecos = "root",
-	.pw_dir = "/root",
-	.pw_shell = "/bin/bash",
-	.pw_uid = 0,
-	.pw_gid = 0,
-};     
-*/
 /*
  * NSS user functions
  */
 
-/* Fetch the next password entry from ntdom password database */
-/*
-nss_status
-_nss_hcp_getpwent_r(struct passwd *result, char *buffer,
-			size_t buflen, int *errnop)
-{
-}
-*/
-/* Return passwd struct from uid */
-/*
-nss_status
-_nss_hcp_getpwuid_r(uid_t uid, struct passwd *result, char *buffer,
-			size_t buflen, int *errnop)
-{
-}
-*/
 /* Return passwd struct from username */
 NSS_STATUS _nss_hcp_getpwnam_r(const char *name, struct passwd *result, char *buffer,
 			size_t buflen, int *errnop)
--- hcp_nss.h
+++ hcp_nss.h
@@ -1,33 +1,14 @@
-/*
-   Unix SMB/CIFS implementation.
-
-   Winbind daemon for ntdom nss module
-
-   Copyright (C) Tim Potter 2000
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 3 of the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#include "common.h"
 #ifndef _HCP_NSS_H
 #define _HCP_NSS_H
-/*
-nss_status _nss_hcp_getpwent_r(struct passwd *result, char *buffer,
-				   size_t buflen, int *errnop);
-nss_status _nss_hcp_getpwuid_r(uid_t uid, struct passwd *result,
-				   char *buffer, size_t buflen, int *errnop);
-*/
+#include <stddef.h>
+#include <nss.h>
+#include <pwd.h>
+#include <grp.h>
+
+typedef enum nss_status NSS_STATUS;
+
+extern NSS_STATUS _nss_winbind_getpwent_r(struct passwd *result, char *buffer, size_t buflen, int *errnop);
+
 NSS_STATUS _nss_hcp_getpwnam_r(const char *name, struct passwd *result,
 				   char *buffer, size_t buflen, int *errnop);
 
